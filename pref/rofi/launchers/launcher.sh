ebf11a020fe2eadc98fa84d887378fae7abd3a88
metnversi <vinh.nt202727@sis.hust.edu.vn>
2024-12-14 23:36:01 +0700

    goodbye copyq

--- pref/rofi/launchers/launcher.sh
+++ pref/rofi/launchers/launcher.sh
@@ -10,7 +10,7 @@
 ## style-1     style-2     style-3     style-4     style-5
 ## style-6     style-7     style-8     style-9     style-10
 
-dir="$HOME/.config/rofi/launchers/my-type"
+dir="$HOME/.config/rofi/launchers"
 theme='style-6'
 #theme='style-7'
 
